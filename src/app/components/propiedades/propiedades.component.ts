d36cc7dc52cddcf9c77659868a5f73650f602afe
EdwinLopezz17 <u20201b051@upc.edu.pe>
2023-06-11 01:07:02 -0500

    Vista de propiedades agregada

--- src/app/components/propiedades/propiedades.component.ts
+++ src/app/components/propiedades/propiedades.component.ts
@@ -4,8 +4,6 @@ import {MatTableDataSource} from "@angular/material/table";
 import {IPropiedades} from "../../models/IPropiedades";
 import {MatSort} from "@angular/material/sort";
 import {MatPaginator} from "@angular/material/paginator";
-import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-propiedades',
   templateUrl: './propiedades.component.html',
